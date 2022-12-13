71a875d7a9d0622c0464f2d374f3a2c7e5a826df
Bfunj <baekhjo9@gmail.com>
2022-12-13 13:40:54 +0900

    AdminIndex 출력 오류 수정

--- jsp/kmarket/src/main/java/kr/co/kmarket/controller/admin/AdminIndexController.java
+++ jsp/kmarket/src/main/java/kr/co/kmarket/controller/admin/AdminIndexController.java
@@ -1,6 +1,7 @@
 package kr.co.kmarket.controller.admin;
 
 import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,19 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@SuppressWarnings("unused")
+
 @WebServlet("/admin/lndex.do")
-public class AdminIndexController {
-private static final long serialVersionUID = 1L;
+public class AdminIndexController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
 	
 	public void init() throws ServletException {
 	}
-	
-	protected void doGet(HttpServletRequest req, HttpServletRequest resp) throws ServletException, IOException {
-		@SuppressWarnings("unused")
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/admin/lndex.jsp");
-		
-	}	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/admin/index.jsp");
+		dispatcher.forward(req, resp);
+	}
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	}
