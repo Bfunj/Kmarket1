1256cce87a14e78fc41c59a5c57db4923455358b
lovelyroaflower <lovelyroaflower@gmail.com>
2022-12-22 09:07:29 +0900

    삭제처리

--- jsp/kmarket/src/main/java/kr/co/kmarket/controller/cs/QnaListController.java
+++ jsp/kmarket/src/main/java/kr/co/kmarket/controller/cs/QnaListController.java
@@ -1,6 +1,7 @@
 package kr.co.kmarket.controller.cs;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,18 +10,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.co.kmarket.service.CsService;
+import kr.co.kmarket.vo.ArticleVO;
+
 @WebServlet("/cs/qna/list.do")
 public class QnaListController extends HttpServlet{
+	
 	private static final long serialVersionUID = 1L;
+	private CsService service = CsService.INSTANCE;
+
 	@Override
 	public void init() throws ServletException {
-		
 	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		
 		String cate = req.getParameter("cate");
+		String pg = req.getParameter("pg");
+		
+		int currentPage = service.getCurrentPage(pg);
+		int total = service.selectCountTotal(cate);
+		int lastPageNum = service.getLastPageNum(total);
+		int limitStart = service.getLimitStart(currentPage);
+		int [] result = service.getPageGroupNum(currentPage, lastPageNum);
+		int pageStartNum = service.getPageStartNum(total, limitStart);
+		int start = service.getStartNum(currentPage);
+		
+		
+		// 페이지 글 가져오기
+		List<ArticleVO> articles = service.selectArticles(cate, start);
+		
+
 		req.setAttribute("cate", cate);
+		req.setAttribute("articles", articles);
+		req.setAttribute("currentPage", currentPage);
+		req.setAttribute("total", total);
+		req.setAttribute("lastPageNum", lastPageNum);
+		req.setAttribute("limitStart", limitStart);
+		req.setAttribute("pageStartNum", pageStartNum+1);
+		req.setAttribute("pageGroupStart", result[0]);
+		req.setAttribute("pageGroupEnd", result[1]);
+		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/cs/qna/list.jsp");
 		dispatcher.forward(req, resp);
 	}
--- jsp/kmarket/src/main/java/kr/co/kmarket/dao/CsDAO.java
+++ jsp/kmarket/src/main/java/kr/co/kmarket/dao/CsDAO.java
@@ -1,9 +1,20 @@
 package kr.co.kmarket.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CsDAO {
+import kr.co.kmarket.db.DBHelper;
+import kr.co.kmarket.db.Sql;
+import kr.co.kmarket.vo.ArticleVO;
+
+public class CsDAO extends DBHelper{
 	private static CsDAO instance = new CsDAO();
 	
 	public static CsDAO getInstance() {
@@ -14,5 +25,114 @@ public class CsDAO {
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
+public int insertArticle(ArticleVO article) {
+		
+		int result = 0;
+		
+		try {
+			
+			logger.info("insertArticle...");
+			
+			Connection con = getConnection();
+			
+			con.setAutoCommit(false);
+			
+			PreparedStatement psmt = con.prepareStatement(Sql.INSERT_ARTICLE);
+			Statement stmt = con.createStatement();
+			
+			psmt.setString(1, article.getCate());
+			psmt.setString(2, article.getTitle());
+			psmt.setString(3, article.getContent());
+			psmt.setString(4, article.getUid());
+			psmt.setString(5, article.getRegip());
+
+			psmt.executeUpdate();
+			ResultSet rs = stmt.executeQuery(Sql.SELECT_MAX_NO);
+
+			con.commit();
+			
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+			
+			rs.close();
+			psmt.close();
+			con.close();
+			
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return result;
+	}
 	
+	// 전체 게시물 개수
+		public int selectCountTotal(String cate) {
+			int total = 0;
+			try {
+				logger.info("selectCountTotal...");
+				
+				Connection conn = getConnection();
+				
+				PreparedStatement psmt = conn.prepareStatement(Sql.SELECT_COUNT_TOTAL);
+				psmt.setString(1, cate);
+				ResultSet rs = psmt.executeQuery();
+				
+				while(rs.next()) {
+					total = rs.getInt(1);
+				}
+				
+				rs.close();
+				psmt.close();
+				conn.close();
+				
+			} catch (Exception e) {
+				logger.error(e.getMessage());
+			}
+			return total;
+		}
+		
+		// 페이지 글 가져오기
+		public List<ArticleVO> selectArticles(String cate, String group, int start){
+			List<ArticleVO> articles = new ArrayList<>();
+			try {
+				logger.info("selectArticles...");
+				
+				Connection conn = getConnection();
+				PreparedStatement psmt = null;
+				
+				if(group == null) {
+					 psmt = conn.prepareStatement(Sql.SELECT_WHOLE_ARTICLES);
+					 psmt.setInt(1, start);
+				}else {
+					psmt = conn.prepareStatement(Sql.SELECT_ARTICLES);
+					psmt.setString(1, cate);
+					psmt.setString(2, group);
+					psmt.setInt(3, start);
+				}
+				
+				ResultSet rs = psmt.executeQuery();
+				while(rs.next()) {
+					ArticleVO article = new ArticleVO();
+					article.setNo(rs.getInt(1));
+					article.setParent(rs.getInt(2));
+					article.setCate(rs.getString(3));
+					article.setTitle(rs.getString(4));
+					article.setContent(rs.getString(5));
+					article.setUid(rs.getString(6));
+					article.setRegip(rs.getString(7));
+					article.setRdate(rs.getString(8));
+					
+					articles.add(article);
+					
+				}
+				rs.close();
+				psmt.close();
+				conn.close();
+				
+			} catch (Exception e) {
+				logger.error(e.getMessage());
+			}
+			return articles;
+		}
+
 }
